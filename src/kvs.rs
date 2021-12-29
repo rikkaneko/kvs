b7c869ad24f4e892ba18f83d6c073a1174e76b21
Joe Ma <rikkaneko23@gmail.com>
2021-12-30 00:04:06 +0800

    Add index file support
    Signed-off-by: Joe Ma <rikkaneko23@gmail.com>

--- src/kvs.rs
+++ src/kvs.rs
@@ -16,18 +16,20 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#![allow(clippy::collapsible_match)]
 use failure::{Error, format_err};
 use std::path::PathBuf;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{BufReader, Seek, SeekFrom, Write};
+use std::io::{BufReader, BufWriter, Seek, SeekFrom, Write};
+use std::os::unix::fs::MetadataExt;
 use serde::{Deserialize, Serialize};
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 pub struct KvStore {
-	file_handle: File,
-	last_pos: u64,
-	entries: HashMap<String, String>
+	file_db_handle: File,
+	file_index_handle: File,
+	index: HashMap<String, u64>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -36,71 +38,136 @@ enum KvEntries {
 	DELETE(String)
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct KvIndexEntries {
+	key: String,
+	offset: u64
+}
+
 impl KvStore {
 	/// Set the value of a string key to a string
 	pub fn set(&mut self, key: String, value: String) -> Result<()> {
-		let entry = KvEntries::SET(key, value);
-		let serialized = bson::to_vec(&entry)?;
-		self.file_handle.seek(SeekFrom::End(0))?;
-		self.file_handle.write_all(serialized.as_slice())?;
+		self.writeback(KvEntries::SET(key, value))?;
 		Ok(())
 	}
 	
 	/// Get the string value of a given string key
 	pub fn get(&mut self, key: String) -> Result<Option<String>> {
-		self.update()?;
-		match self.entries.get(&key) {
-			Some(value) => Ok(Some(value.clone())),
+		match self.index.get(&key) {
+			Some(value) => {
+				let mut reader = BufReader::new(&mut self.file_db_handle);
+				reader.seek(SeekFrom::Start(*value))?;
+				if let Ok(entry) = bson::from_reader::<_, KvEntries>(reader) {
+					if let KvEntries::SET(key_, value) = entry {
+						if key == key_ { return Ok(Some(value)) }
+					}
+				}
+				// Invalid index happened! Reindex and retry once
+				// The database file has been modified externally.
+				// Should not be here
+				panic!("Unknown I/O error.");
+			},
 			None => Ok(None)
 		}
 	}
 	
 	/// Remove a given key `key`
 	pub fn remove(&mut self, key: String) -> Result<()> {
-		self.update()?;
-		if !self.entries.contains_key(&key) { return Err(format_err!("Key not found")) }
-		let entry = KvEntries::DELETE(key);
-		let serialized = bson::to_vec(&entry)?;
-		self.file_handle.seek(SeekFrom::End(0))?;
-		self.file_handle.write_all(serialized.as_slice())?;
+		if self.get(key.clone())?.is_none() { return Err(format_err!("Key not found")) }
+		self.writeback(KvEntries::DELETE(key))?;
 		Ok(())
 	}
 	
-	/// Create new KvStore from file
+	/// Create or open KvStore instance
 	pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
-		let mut path = path.into();
-		if path.is_dir() { path = path.join("kvs.db") }
-		let file_handle = OpenOptions::new().read(true).append(true).create(true).open(path)?;
+		let mut db_path = path.into();
+		let mut index_path = db_path.clone();
+		if db_path.is_dir() {
+			db_path = db_path.join("kvs.db");
+			index_path = index_path.join("kvs.dir");
+		} else {
+			index_path = index_path.with_extension("dir");
+		}
+		
+		let mut file_db_handle = OpenOptions::new().read(true).append(true).create(true).open(db_path)?;
+		let file_index_handle = OpenOptions::new().read(true).append(true).create(true).open(index_path)?;
+		// Reindex if index file not exist or has zero length
+		let mut index = if file_index_handle.metadata()?.size() == 0 {
+			KvStore::reindex(&mut file_db_handle)?
+		} else { HashMap::new() };
+		
+		let mut reader = BufReader::new(file_index_handle.try_clone()?);
+		// Build index from existing index file
+		while let Ok(entry) = bson::from_reader::<_, KvIndexEntries>(&mut reader) {
+			index.insert(entry.key, entry.offset);
+		}
+		
 		Ok(KvStore {
-			file_handle,
-			last_pos: 0,
-			entries: HashMap::new()
+			file_db_handle,
+			file_index_handle,
+			index
 		})
 	}
 	
-	/// Update in-memory cache to latest change of the database file
-	fn update(&mut self) -> Result<()> {
-		let mut reader = BufReader::new(self.file_handle.try_clone()?);
-		reader.seek(SeekFrom::Start(self.last_pos))?;
+	/// Re-index the database file
+	fn reindex(file_db_handle: &mut File) -> Result<HashMap<String, u64>> {
+		let mut offset = 0;
+		let mut index = HashMap::new();
+		let mut reader = BufReader::new(file_db_handle);
+		reader.seek(SeekFrom::Start(0))?;
 		while let Ok(entry) = bson::from_reader::<_, KvEntries>(&mut reader) {
 			match entry {
-				KvEntries::SET(key, value) => { self.entries.insert(key, value); },
-				KvEntries::DELETE(key) => { self.entries.remove(&key); }
+				KvEntries::SET(key, _) => { index.insert(key, offset); },
+				KvEntries::DELETE(key) => { index.remove(&key); }
 			}
+			// Store the start offset of next entry
+			offset = reader.seek(SeekFrom::Current(0))?;
 		}
-		self.last_pos = reader.seek(SeekFrom::Current(0))?;
-		Ok(())
+		Ok(index)
 	}
 	
 	/// Do compaction if the database file size reaches threshold
 	fn compaction(&mut self) -> Result<()> {
 		todo!()
 	}
+	
+	fn writeback(&mut self, entry: KvEntries) -> Result<()> {
+		let serialized = bson::to_vec(&entry)?;
+		let offset = self.file_db_handle.seek(SeekFrom::End(0))?;
+		self.file_db_handle.write_all(serialized.as_slice())?;
+		match entry {
+			KvEntries::SET(key, _) => { self.index.insert(key, offset); },
+			KvEntries::DELETE(key) => { self.index.remove(&key); }
+		}
+		Ok(())
+	}
+	
+	fn write_index(&mut self) -> Result<()> {
+		self.file_index_handle.set_len(0)?;
+		let mut writer = BufWriter::new(&mut self.file_index_handle);
+		// Truncate file won't change the cursor
+		writer.seek(SeekFrom::Start(0))?;
+		for (key, offset) in self.index.iter() {
+			let entry = KvIndexEntries {
+				key: key.clone(),
+				offset: *offset
+			};
+			let serialized = bson::to_vec(&entry)?;
+			writer.write_all(serialized.as_slice())?;
+		}
+		Ok(())
+	}
+}
+
+impl Drop for KvStore {
+	fn drop(&mut self) {
+		self.write_index().unwrap();
+	}
 }
 
 #[test]
 fn test_kv() -> Result<()> {
-	let mut store = KvStore::open("/tmp/kvs.db")?;
+	let mut store = KvStore::open("/tmp")?;
 	store.set("user.root.password".to_string(), "archlinuxisthebest".to_string())?;
 	store.set("user.root.name".to_string(), "系統管理員".to_string())?;
 	store.set("user.root.balance".to_string(), "50000".to_string())?;
